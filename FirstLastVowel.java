ec176cb095ac3bf53d98be5dc798a7c0d648871a
lucaszhuang1210 <74164114+lucaszhuang1210@users.noreply.github.com>
2021-01-05 17:39:24 -0800

    Update FirstLastVowel.java

--- FirstLastVowel.java
+++ FirstLastVowel.java
@@ -1,13 +1,21 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Lucas Zhuang
+//Date - 1/5/2021
 
 
 public class FirstLastVowel
 {
    public static String go( String a )
  {
-  return "add code here";
+      String first = a.charAt(0);
+      String last = a.charAt(a.length-1);
+      if(first == "a"||first == "e"||first == "i"||first == "o"||first == "u"||first == "A"||first == "E"||first == "I"||first == "O"||first == "U")
+      {
+         return "yes";
+      }else if(last == "a"||last == "e"||last == "i"||last == "o"||last == "u"||last == "A"||last == "E"||last == "I"||last == "O"||last == "U"){
+         return "yes";
+      }
+      return no;
  }
 }
